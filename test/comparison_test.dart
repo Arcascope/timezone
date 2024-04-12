db2badadf4f714946935a9dffdea89b4843b7411
ericcanton <57836255+ericcanton@users.noreply.github.com>
2024-04-12 10:00:55 -0500

    add unit test proving issue for separate Isolate, and proving that refactored hashCode and == works across isolates.

--- test/comparison_test.dart
+++ test/comparison_test.dart
@@ -1,18 +1,40 @@
 @TestOn('vm')
 library;
 
+import 'dart:isolate';
+
 import 'package:test/test.dart';
 import 'package:timezone/standalone.dart';
 
 Future<void> main() async {
   await initializeTimeZone();
-  group('Test equality and inequality', () {
+  group('Equality and inequality of Timezones', () {
     test('Test that == checks for the same moment in time, not location', () {
-      final location1 = getLocation('Europe/London');
-      final location2 = getLocation('America/Detroit');
+      final location1 = getLocation('Europe/London'); // UTC+0 on 2021-01-01
+      final location2 = getLocation('America/Detroit'); // UTC-5 on 2021-01-01
       final time1 = TZDateTime(location1, 2021, 1, 1, 5, 0);
       final time2 = TZDateTime(location2, 2021, 1, 1, 0, 0);
       expect(time1, time2);
+    });
+
+    test('Test that != checks for the same moment in time, not location', () {
+      final location1 = getLocation('Europe/London');
+      final location2 = getLocation('America/Detroit');
+      final time1 = TZDateTime(location1, 2021, 1, 1, 20, 0);
+      final time2 = TZDateTime(location2, 2021, 1, 1, 0, 0);
+      expect(time1 != time2, isTrue);
+    });
+  });
+
+  group('Equality of Locations', () {
+    test('Test that Locations initialized on different threads are equal',
+        () async {
+      final location1 = getLocation('Europe/London');
+      final location2 = await Isolate.run(() async {
+        await initializeTimeZone();
+        return getLocation('Europe/London');
+      });
+      expect(location1, location2);
     });
   });
 }
